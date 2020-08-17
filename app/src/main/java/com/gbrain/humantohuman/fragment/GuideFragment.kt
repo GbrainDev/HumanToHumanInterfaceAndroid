5ed1b05af49ed02f212762d9dd78a991e41adf95
SangwooJ <ndrw5580@gmail.com>
2020-08-18 02:40:18 +0900

    update guide ENG/KOR language

--- app/src/main/java/com/gbrain/humantohuman/fragment/GuideFragment.kt
+++ app/src/main/java/com/gbrain/humantohuman/fragment/GuideFragment.kt
@@ -1,12 +1,16 @@
 package com.gbrain.humantohuman.fragment
 
+import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import com.gbrain.humantohuman.R
+import kotlinx.android.synthetic.main.fragment_guide.*
 import kotlinx.android.synthetic.main.fragment_guide.view.*
+import kotlinx.android.synthetic.main.fragment_main.*
 import kr.co.prnd.YouTubePlayerView
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,6 +22,8 @@ import kr.co.prnd.YouTubePlayerView
  * create an instance of this fragment.
  */
 class GuideFragment : Fragment() {
+
+    private var lang = 0 // 0:kor 1:eng
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,6 +37,18 @@ class GuideFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val youTubePlayerView: YouTubePlayerView = view.you_tube_player_view
         youTubePlayerView.play("fGMDq3ay7Ro")
+        btn_lang.setOnClickListener {
+            if (lang == 0) {
+                txt_guide.text = getString(R.string.guide_description_kor)
+                btn_lang.text= "English"
+                lang = 1
+            }
+            else {
+                txt_guide.text = getString(R.string.guide_description_eng)
+                btn_lang.text= "한국어"
+                lang = 0
+            }
+        }
     }
 
 
