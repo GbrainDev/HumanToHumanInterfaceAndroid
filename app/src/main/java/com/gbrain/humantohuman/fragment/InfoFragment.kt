e96942e5c4dd7ad672af2a05412d504f65c6520a
binchoo <079111w@gmail.com>
2020-08-25 17:09:12 +0900

    Add alert dialog for license display.

--- app/src/main/java/com/gbrain/humantohuman/fragment/InfoFragment.kt
+++ app/src/main/java/com/gbrain/humantohuman/fragment/InfoFragment.kt
@@ -3,9 +3,12 @@ package com.gbrain.humantohuman.fragment
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.gbrain.humantohuman.R
 import kotlinx.android.synthetic.main.fragment_chart.*
@@ -15,27 +18,19 @@ private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
 class InfoFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    var container: ViewGroup? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        this.container = container
         return inflater.inflate(R.layout.fragment_info, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setupGUI()
     }
 
     private fun setupGUI() {
@@ -45,16 +40,30 @@ class InfoFragment : Fragment() {
             intent.setData(uri);
             startActivity(intent);
         }
+
+        license_button.setOnClickListener {
+            container?.also {
+                val builder = AlertDialog.Builder(requireContext(), android.R.style.ThemeOverlay_Material_Dialog_Alert)
+                builder.setTitle("Open Source Licenses.txt")
+                    .setNeutralButton("Close", null)
+                    .setView(loadLicenseView())
+                builder.create().show()
+            }
+        }
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InfoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun loadLicenseView(): View {
+        val inflater = LayoutInflater.from(requireActivity())
+        val licenseView = inflater.inflate(R.layout.layout_license_viewer, container!!, false)
+        val licenseTextView = licenseView.findViewById<TextView>(R.id.license_text)
+        licenseTextView.setText(loadLicenseContents())
+        return licenseView
+    }
+
+    private fun loadLicenseContents(): String {
+        val fileInputStream = resources.openRawResource(R.raw.license_enumeration)
+        val contents = fileInputStream.readBytes()
+        fileInputStream.close()
+        return String(contents)
     }
 }
