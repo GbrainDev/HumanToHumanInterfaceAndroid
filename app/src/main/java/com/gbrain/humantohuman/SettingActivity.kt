6db604f708ba6387d392826ab88f2a624374a2ba
binchoo <079111w@gmail.com>
2020-08-26 10:21:03 +0900

    Add title for SettingActivity. Add redundant hint to improve UX.

--- app/src/main/java/com/gbrain/humantohuman/SettingActivity.kt
+++ app/src/main/java/com/gbrain/humantohuman/SettingActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_setting.*
 
 
